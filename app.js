bb94c312b0fbcd345b79ebe043637cbfcfd41bd7
ibnul42 <ibnulashir42@gmail.com>
2021-06-15 19:43:12 +0600

    api initialized for data

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const Twitter = require('./api/helpers/twitter');
+const twitter = new Twitter();
+require('dotenv').config()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
+    
+app.get('/tweets', (req, res) => {
+    const query = req.query.q;
+    const count = req.query.count;
+    console.log(process.env.TWITTER_API_TOKEN)
+
+    twitter.get(query, count).then((response) => {
+        res.status(200).send(response.data)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
