85b0c8781e1b8c272349121626b1f396e3500a19
ibnul42 <ibnulashir42@gmail.com>
2021-06-17 22:49:00 +0600

    Full version of Twitter Clone Lite

--- api/helpers/twitter.js
+++ api/helpers/twitter.js
@@ -4,11 +4,13 @@ const URL = "https://api.twitter.com/1.1/search/tweets.json";
 
 class Twitter {
 
-    get(query, count) {
+    get(query, count, maxId) {
         return axios.get(URL, {
             params: {
                 q: query,
-                count: count
+                count: count,
+                tweet_mode: "extended",
+                max_Id: maxId
             },
             headers: {
                 "Authorization": `Bearer ${process.env.TWITTER_API_TOKEN}`
--- app.js
+++ app.js
@@ -14,9 +14,8 @@ app.use(function(req, res, next) {
 app.get('/tweets', (req, res) => {
     const query = req.query.q;
     const count = req.query.count;
-    console.log(process.env.TWITTER_API_TOKEN)
-
-    twitter.get(query, count).then((response) => {
+    const maxId = req.query.max_Id;
+    twitter.get(query, count, maxId).then((response) => {
         res.status(200).send(response.data)
     }).catch((error) => {
         res.status(400).send(error)
